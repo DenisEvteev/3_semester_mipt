1212dfa7fc3f114faca8b0f43a1749938de0b782
eda_3802 <evteev.da@phystech.edu>
2019-12-12 16:10:17 +0300

    2_VERS_INCREASE_POSS_CHILDREN

--- os_course/server/main.c
+++ os_course/server/main.c
@@ -10,8 +10,6 @@
 #include <assert.h>
 #include <math.h>
 
-#define NORETURN __attribute__((noreturn))
-
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
 #define error(str)                                              \
@@ -59,29 +57,27 @@ typedef struct link_t {
     int cur_max_fd;
 } link_t;
 
-enum
-{
+enum {
     BASE = 10,
-    EMPTY_FD = -1,
-    SECONDS = 5,
-    M_SECONDS = 0
+    EMPTY_FD = -1
 };
 
 enum SIZE {
     CHILD_BUF_SIZE = 128000,
-    BUF_SIZE = 1024 // min capacity for the parent buffer
+    BUF_SIZE = 1024
 };
 
-void
-child(int fd_read, int fd_write) NORETURN;
+int
+child(int fd_read, int fd_write);
 
-void
+int
 host(link_t *connections, unsigned n);
 
 size_t
 buf_size(unsigned pow_i);
 
-link_t *create_array_pipes(size_t n);
+void close_low_descpts(unsigned up_limit, link_t *connections);
+
 
 int
 main(int argc, char *argv[]) {
@@ -102,41 +98,50 @@ main(int argc, char *argv[]) {
     if (fd == -1)
         error("error in opening file with data");
 
-    //create all the pipes in advance in parent process
-    link_t *connections = create_array_pipes(n);
+    link_t *connections = calloc(n, sizeof(link_t));
+    assert(connections);
 
     pid_t pid = 0;
     int i;
     for (i = 0; i < n; ++i) {
+        if (pipe(connections[i].p_in) == -1)
+            error("error in pipe");
+
         pid = fork();
 
-        /*Child close descriptors that it won't use at all*/
         if (pid == 0) {
-            if (i != 0)
-                c_c(connections[i - 1].p_out[1]);
+            close_low_descpts(i, connections);
             c_c(connections[i].p_in[0]);
             break;
-        }
+        } else if (pid > 0) {
+            if (!i)
+                c_c(fd);
 
-        else if (pid > 0)
-        {
             c_c(connections[i].p_in[1]);
+
+            if (i == n - 1) {
+                connections[i].p_out[1] = STDOUT_FILENO;
+                connections[i].p_out[0] = EMPTY_FD;
+
+            } else {
+                if (pipe(connections[i].p_out) == -1)
+                    error("error in pipe");
+            }
 
             connections[i].cur_max_fd = max(connections[i].p_in[0], connections[i].p_out[1]);
             if (i != 0 && connections[i].cur_max_fd < connections[i - 1].cur_max_fd)
                 connections[i].cur_max_fd = connections[i - 1].cur_max_fd;
+
         }
 
         if (pid == -1)
             error("error in fork");
     }
 
-    /*The continue of the child process*/
-    if (!pid)
+    //children
+    if (!pid) {
         i ? child(connections[i - 1].p_out[0], connections[i].p_in[1]) : child(fd, connections[i].p_in[1]);
-
-    else {
-        /*close the rest part of the descriptors in the parent which will be useless in it*/
+    } else {
         for (int i = 0; i < n; ++i) {
             if (i != n - 1)
                 c_c(connections[i].p_out[0]);
@@ -145,36 +150,24 @@ main(int argc, char *argv[]) {
         host(connections, n);
     }
 
+    free(connections);
     return 0;
 }
 
-link_t *create_array_pipes(size_t n) {
-    assert(n > 0);
-    link_t *connections = calloc(n, sizeof(link_t));
-    assert(connections);
+void close_low_descpts(unsigned up_limit, link_t *connections) {
+    assert(up_limit >= 0 && connections);
 
-    for (int i = 0; i < n; ++i) {
-        if (pipe(connections[i].p_in) == -1)
-            error("error in pipe");
-        if (i == n - 1) {
-            connections[i].p_out[1] = STDOUT_FILENO;
-            connections[i].p_out[0] = EMPTY_FD;
-        } else {
-            if (pipe(connections[i].p_out) == -1)
-                error("error in pipe");
-        }
+    for (int i = 0; i < up_limit; ++i) {
+        c_c(connections[i].p_in[0]);
+        c_c(connections[i].p_out[1]);
+        if (i != up_limit - 1)
+            c_c(connections[i].p_out[0]);
     }
-    return connections;
 }
 
-void
+int
 host(link_t *connections, unsigned n) {
     assert(connections && n > 0);
-    fd_set rd, wr;
-    struct timeval timeout;
-    timeout.tv_sec = SECONDS;
-    timeout.tv_usec = M_SECONDS;
-
 
     for (int i = 0; i < n; ++i) {
         o_non(connections[i].p_in[0]);
@@ -187,96 +180,108 @@ host(link_t *connections, unsigned n) {
         assert(connections[i].buf);
     }
 
-    /*This variables are used to determine when I must go out from the
-     * endless loop --- I mean that the case of their equality we consider to be the point when
-     * we should leave the loop*/
-    off_t into = 0;
-    off_t from = 0;
+    size_t into = 0;
+    size_t from = 0;
 
-    //int ip_ = 0;
+
+    int death_first_ch = 0;
+    ssize_t write_b = 0;
+
     for (;;) {
+        fd_set rd, wr;
         FD_ZERO(&rd);
         FD_ZERO(&wr);
-//
-//        if(ip_ == n)
-//            --ip_;
+
 
         for (int i = 0; i < n; ++i) {
-            FD_SET(connections[i].p_in[0], &rd);
-            FD_SET(connections[i].p_out[1], &wr);
+            if (connections[i].size_ == 0) {
+                if (i == 0) {
+                    if (death_first_ch == 0)
+                        FD_SET(connections[0].p_in[0], &rd);
+                } else
+                    FD_SET(connections[i].p_in[0], &rd);
+            }
+
+            if (connections[i].size_ != 0) {
+                FD_SET(connections[i].p_out[1], &wr);
+            }
         }
 
-        int number_fd = select(connections[n - 1].cur_max_fd + 1, &rd, &wr, NULL, &timeout);
+        int number_fd = select(connections[n - 1].cur_max_fd + 1, &rd, &wr, NULL, NULL);
         if (number_fd == -1)
             error("error in select");
 
         int finished_operations = 0;
         int counter = 0;
-        ssize_t write_b = 0;
-        while (finished_operations != number_fd)
-        {
+
+        while (finished_operations != number_fd) {
+
             if (FD_ISSET(connections[counter].p_in[0], &rd)) {
-                /*My host won't write to the host buffer until the size of it becomes 0 */
-                if (connections[counter].size_ == 0) {
-                    connections[counter].size_ = read(connections[counter].p_in[0], connections[counter].buf,
-                                                      connections[counter].cap_);
-                    if (connections[counter].size_ == -1)
-                        error("read error");
-                    if (counter == 0)
-                        into += connections[counter].size_;
+
+                connections[counter].size_ = read(connections[counter].p_in[0], connections[counter].buf,
+                                                  connections[counter].cap_);
+
+
+                if (connections[counter].size_ == -1)
+                    error("read error");
+
+                if (counter == 0) {
+                    into += connections[counter].size_;
+
+                    if (connections[counter].size_ == 0)
+                        death_first_ch = 1;
                 }
+
+                if (connections[counter].size_ == 0 && into == 0)
+                    break;
 
                 ++finished_operations;
             }
 
             if (FD_ISSET(connections[counter].p_out[1], &wr)) {
-                if (connections[counter].size_ != 0) {
-                    /*Here we write to pipe so we want the bad behaviour of write with o_NONBLOCK
-                     * When I've understood that it will be very nice to write BUF_SIZE bytes in the ready for writing descriptor
-                     * I've was very glad to write this comment and the code further !!!
-                     * Because I've understood the relationship between the ready for writing descriptor
-                     * and the current number of bytes in the pipe@@@
-                     * It's so tasty and beautiful*/
-                    write_b = write(connections[counter].p_out[1],
-                                    connections[counter].buf + connections[counter].off_wr,
-                                    BUF_SIZE > connections[counter].size_ ? connections[counter].size_
-                                                                          : BUF_SIZE); //BUF_SIZE == 1024
-                    if (write_b == -1)
-                        error("error in write");
-                    connections[counter].off_wr += write_b;
-                    connections[counter].size_ -= write_b;
-                    if (connections[counter].size_ == 0)
-                        connections[counter].off_wr = 0;
 
-                    if (counter == n - 1)
-                        from += write_b;
-                }
+                write_b = write(connections[counter].p_out[1],
+                                connections[counter].buf + connections[counter].off_wr,
+                                BUF_SIZE > connections[counter].size_ ? connections[counter].size_
+                                                                      : BUF_SIZE); //BUF_SIZE == 1024
+                if (write_b == -1)
+                    error("error in write");
+
+                connections[counter].off_wr += write_b;
+                connections[counter].size_ -= write_b;
+
+                if (connections[counter].size_ == 0)
+                    connections[counter].off_wr = 0;
+
+                if (counter == n - 1)
+                    from += write_b;
+
                 ++finished_operations;
-
             }
 
             ++counter;
         }
 
-        if (from && from == into && connections[counter - 1].size_ == 0 && write_b == 0)
+        if ((from && (from == into) && death_first_ch) ||
+            (connections[counter].size_ == 0 && into == 0)) {
             break;
-        //++ip_;
+        }
+
     }
 
-    for (int j = 0; j < n; ++j)
-    {
+    for (int j = 0; j < n; ++j) {
         c_c(connections[j].p_in[0]);
         c_c(connections[j].p_out[1]);
         free(connections[j].buf);
     }
-
-    free(connections);
+    return 0;
 }
 
-void
+
+int
 child(int fd_read, int fd_write) {
     assert(fd_read >= 0 && fd_write >= 0);
-    char buf[PIPE_BUF] = {};
+    char *buf = calloc(PIPE_BUF, sizeof(char));
 
     ssize_t bytes_r = 0;
     ssize_t bytes_w = 0;
@@ -293,12 +298,12 @@ child(int fd_read, int fd_write) {
 
     c_c(fd_read);
     c_c(fd_write);
-    exit(EXIT_SUCCESS);
+    free(buf);
+    return 0;
 }
 
 size_t
-buf_size(unsigned pow_i)
-{
+buf_size(unsigned pow_i) {
     errno = 0;
     double size = pow(3, (double) (pow_i));
     if (errno != 0)
